c305a7042e74ec9d9b5314d25ae63b4f3432d27b
LCAR979 <tcoperator@163.com>
2015-10-27 10:40:18 -0300

    First commit, with bundle installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HITCrs::Application.config.secret_token = '6f4897a17433e331daaea8e7c5e745153cfe83de65bcc22e49c95f36de1243ca88a801f671e4ab830e74fc21539aaaaae868673f894869157b2193de00098eb9'
