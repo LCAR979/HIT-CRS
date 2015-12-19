dfa000cf59d82b1a3c687ac83852ca5a72c3435e
LCAR979 <tcoperator@163.com>
2015-12-19 00:16:00 -0400

    sendgrid added, production mail test start

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+ActionMailer::Base.smtp_settings = {
+	:address => "smtp.sendgrid.net",     
+    :port  => '587',       
+    :domain => 'heroku.com'  
+    :user_name  => ENV['SENDGRID_USERNAME']     
+    :password   => ENV['SENDGRID_PASSWORD']     
+    :authentication  => 'plain',     
+    :raise_delivery_errors => true,     
+    :enable_starttls_auto => true 
+  }
 # Initialize the rails application
 HITCrs::Application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,12 @@ HITCrs::Application.configure do
   Rails.application.routes.default_url_options[:host] = 'localhost'   
   config.action_mailer.delivery_method = :smtp   
   config.action_mailer.perform_deliveries = true   
-  config.action_mailer.smtp_settings = {:address => "smtp.163.com",     
-    :port  => 25,     
-    :host  => 'localhost',     
-    :user_name  => 'shuangchen1994',     
-    :password   =>  'xx',     
+  config.action_mailer.smtp_settings = {:address => "smtp.sendgrid.net",     
+    :port  => '587',     
+    :host  => 'localhost',   
+    :domain => 'heroku.com'  
+    :user_name  => ENV['SENDGRID_USERNAME']     
+    :password   => ENV['SENDGRID_PASSWORD']     
     :authentication  => 'plain',     
     :raise_delivery_errors => true,     
     :enable_starttls_auto => true 
