f46a8e63a2d21ff0a5cf4b6631c5f256782bf942
LCAR979 <tcoperator@163.com>
2015-12-19 08:30:36 -0400

    change private method

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,15 +2,17 @@ class Applicant < ActiveRecord::Base
 	before_create :confirmation_token
 	attr_accessible :username, :studentid, :phone, :department, :email, :name, :password,:isvalid
 	has_many :requests, dependent: :destroy
-	private
+	#private
+
 	def confirmation_token
 		if self.confirmation_token.blank?
 			self.confirmation_token = SecureRandom.urlsafe_base64.to_s
 		end
 	end
+
 	def email_active
 		self.email_confirmed = true
 		self.confirm_token = nil
-		#save!(:validate => false)
 	end
+
 end
