b4711d518c1b07f42d28a0e5206f8bb960b8f6c8
LCAR979 <tcoperator@163.com>
2015-12-19 00:42:51 -0400

    fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,15 +64,15 @@ HITCrs::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  Rails.application.routes.default_url_options[:host] = 'localhost'   
+  Rails.application.routes.default_url_options[:host] = 'www.hticrs.herokuapp.com'   
   config.action_mailer.delivery_method = :smtp   
   config.action_mailer.perform_deliveries = true   
-  config.action_mailer.smtp_settings = {:address => "smtp.sendgrid.net",     
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.sendgrid.net",     
     :port  => '587',     
-    :host  => 'localhost',   
-    :domain => 'heroku.com'  
-    :user_name  => ENV['SENDGRID_USERNAME']     
-    :password   => ENV['SENDGRID_PASSWORD']     
+    :domain => 'heroku.com',  
+    :user_name  => ENV['SENDGRID_USERNAME'],    
+    :password   => ENV['SENDGRID_PASSWORD'],    
     :authentication  => 'plain',     
     :raise_delivery_errors => true,     
     :enable_starttls_auto => true 
