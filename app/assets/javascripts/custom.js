a082a8be21fd5347a7def2701c6f2bf0764dc225
LCAR979 <tcoperator@163.com>
2015-12-21 05:28:54 -0400

    precompile

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $('.alert').remove();
-  }, 5000);
- })
+// $(document).ready(function(){
+//   setTimeout(function(){
+//     $('.alert').remove();
+//   }, 5000);
+//  })
+function change(){
+        var st=document.getElementsByName("identity");
+		var dep=document.getElementById("dep");
+		var stNum=document.getElementById("stNum");
+		var staffNum=document.getElementById("staffNum");
+        if(st[1].checked==true){
+			dep.style.display="none";
+			stNum.style.display="none";
+			staffNum.style.display="block";
+        }
+		else{
+			dep.style.display="block";
+			stNum.style.display="block";
+			staffNum.style.display="none";
+		}
+    }
